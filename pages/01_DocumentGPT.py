9e5c960b92a006c4eb0e7d1962683f14406965ff
samsepi0l-4172 <turtlefrog0811@icloud.com>
2024-06-18 18:36:43 +0900

    chore: Update Home.py with new imports, set page configuration, and add prompt template and selectbox code

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -6,6 +6,7 @@ from langchain.storage import LocalFileStore
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores.faiss import FAISS
 from langchain.chat_models import ChatOpenAI
+from langchain.callbacks.base import BaseCallbackHandler
 import streamlit as st
 
 st.set_page_config(
@@ -13,8 +14,25 @@ st.set_page_config(
     page_icon="📃",
 )
 
+class ChatCallbackHandler(BaseCallbackHandler):
+    message = ""
+
+    def on_llm_start(self, *args, **kwargs):
+        self.message_box = st.empty()
+
+    def on_llm_end(self, *args, **kwargs):
+        save_message(self.message, "ai")
+
+    def on_llm_new_token(self, token, *args, **kwargs):
+        self.message += token
+        self.message_box.markdown(self.message)
+
 llm = ChatOpenAI(
     temperature=0.1,
+    streaming=True,
+    callbacks=[
+        ChatCallbackHandler(),
+    ],
 )
 
 @st.cache_data(show_spinner="Embedding file...")
@@ -37,11 +55,14 @@ def embed_file(file):
     retriever = vectorstore.as_retriever()
     return retriever
 
+def save_message(message, role):
+    st.session_state["messages"].append({"message": message, "role": role})
+
 def send_message(message, role, save=True):
     with st.chat_message(role):
         st.markdown(message)
     if save:
-        st.session_state["messages"].append({"message": message, "role": role})
+        save_message(message, role)
 
 def paint_history():
     for message in st.session_state["messages"]:
@@ -77,7 +98,7 @@ Welcome!
 Use this chatbot to ask questions to an AI about your files!
 
 Upload your files on the sidebar.
-    """
+"""
 )
 
 with st.sidebar:
@@ -101,8 +122,7 @@ if file:
             | prompt
             | llm
         )
-        response = chain.invoke(message)
-        send_message(response.content, "ai")
-
+        with st.chat_message("ai"):
+            response = chain.invoke(message)
 else:
     st.session_state["messages"] = []
