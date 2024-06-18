3362628f3f072bd8afd0d6517323a2ca56fad218
samsepi0l-4172 <turtlefrog0811@icloud.com>
2024-06-18 16:59:32 +0900

    Update Home.py with new imports and set page configuration

--- Home.py
+++ Home.py
@@ -1,27 +1,9 @@
 import streamlit as st
-from datetime import datetime
 
-today = datetime.today().strftime("%H:%M:%S")
-st.title(today)
-
-model = st.selectbox(
-    "Choose your model",
-    (
-        "GPT-3",
-        "GPT-4"
-    )
+st.set_page_config(
+    page_title="FullstackGPT Home",
+    page_icon="🧊",
 )
-if model == "GPT-3":
-    st.write("cheap")
-else:
-    st.write("not cheap")
 
-    name = st.text_input("What is your name?")
-    st.write(name)
+st.title("FullstackGPT Home")
 
-    value = st.slider(
-        "temperature",
-        min_value= 0.1,
-        max_value= 1.0
-    )
-    st.write(value)
