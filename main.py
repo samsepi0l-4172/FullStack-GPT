a2540b14433996e7d3bba320aef1ad7862b1c778
samsepi0l-4172 <turtlefrog0811@icloud.com>
2024-06-27 17:03:36 +0900

    Refactor .gitignore to include movies.sqlite and recipes.csv

--- main.py
+++ main.py
@@ -1,85 +1,30 @@
-from typing import Any, Dict
-from fastapi import Body, FastAPI, Form, Request
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI
 from pydantic import BaseModel, Field
-from dotenv import load_dotenv
-import pinecone
-import os
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import pinecone as Pinecone
-
-load_dotenv()
-
-pinecone.init(
-    api_key=os.getenv("PINECONE_API_KEY"),
-    environment="gcp-starter",
-)
-
-embeddings = OpenAIEmbeddings()
-
-vector_store = Pinecone.from_existing_index(
-    "recipes",
-    embeddings,
-)
-
 
 app = FastAPI(
-    title="CheftGPT. The best provider of Indian Recipes in the world.",
-    description="Give ChefGPT the name of an ingredient and it will give you multiple recipes to use that ingredient on in return.",
-    servers=[
-        {
-            "url": "https://occupations-partition-governments-analyzed.trycloudflare.com",
-        },
-    ],
+    title="Nicolacus Maximus Quote Giver",
+    description="Get a real quote said by Nicolacus Maximus himself.",
 )
 
 
-class Document(BaseModel):
-    page_content: str
+class Quote(BaseModel):
+    quote: str = Field(
+        description="The quote that Nicolacus Maximus said.",
+    )
+    year: int = Field(
+        description="The year when Nicolacus Maximus said the quote.",
+    )
 
 
 @app.get(
-    "/recipes",
-    summary="Returns a list of recipes.",
-    description="Upon receiving an ingredient, this endpoint will return a list of recipes that contain that ingredient.",
-    response_description="A Document object that contains the recipe and preparation instructions",
-    response_model=list[Document],
-    openapi_extra={
-        "x-openai-isConsequential": False,
-    },
+    "/quote",
+    summary="Returns a random quote by Nicolacus Maximus",
+    description="Upon receiving a GET request this endpoint will return a real quiote said by Nicolacus Maximus himself.",
+    response_description="A Quote object that contains the quote said by Nicolacus Maximus and the date when the quote was said.",
+    response_model=Quote,
 )
-def get_recipe(ingredient: str):
-    docs = vector_store.similarity_search(ingredient)
-    return docs
-
-
-user_token_db = {"ABCDEF": "nico"}
-
-
-@app.get(
-    "/authorize",
-    response_class=HTMLResponse,
-    include_in_schema=False,
-)
-def handle_authorize(client_id: str, redirect_uri: str, state: str):
-    return f"""
-    <html>
-        <head>
-            <title>Nicolacus Maximus Log In</title>
-        </head>
-        <body>
-            <h1>Log Into Nicolacus Maximus</h1>
-            <a href="{redirect_uri}?code=ABCDEF&state={state}">Authorize Nicolacus Maximus GPT</a>
-        </body>
-    </html>
-    """
-
-
-@app.post(
-    "/token",
-    include_in_schema=False,
-)
-def handle_token(code=Form(...)):
+def get_quote():
     return {
-        "access_token": user_token_db[code],
+        "quote": "Life is short so eat it all.",
+        "year": 1950,
     }
