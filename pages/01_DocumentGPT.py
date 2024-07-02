059b1833673d011defded7db77303946f1b2a518
samsepi0l-4172 <turtlefrog0811@icloud.com>
2024-07-02 18:00:56 +0900

    Refactor imports and code organization in 01_DocumentGPT.py

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -1,11 +1,12 @@
-from langchain.prompts import ChatPromptTemplate
-from langchain.document_loaders import UnstructuredFileLoader
-from langchain.embeddings import CacheBackedEmbeddings, OpenAIEmbeddings
-from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_community.document_loaders import UnstructuredFileLoader
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain.embeddings.cache import CacheBackedEmbeddings
+from langchain_core.runnables import RunnableLambda, RunnablePassthrough
 from langchain.storage import LocalFileStore
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.vectorstores.faiss import FAISS
-from langchain.chat_models import ChatOpenAI
+from langchain_community.vectorstores import FAISS
+from langchain_openai import ChatOpenAI
 from langchain.callbacks.base import BaseCallbackHandler
 import streamlit as st
 
