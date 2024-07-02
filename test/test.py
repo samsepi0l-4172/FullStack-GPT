982dd2c1a39436c9bef16f58d55137126bfc749b
samsepi0l-4172 <turtlefrog0811@icloud.com>
2024-07-02 14:40:23 +0900

    Refactor code organization and add ChatOpenAI import

--- test/test.py
+++ test/test.py
@@ -2,6 +2,11 @@ import pydantic
 from langchain_openai import ChatOpenAI
 from langchain.callbacks import StreamingStdOutCallbackHandler
 from langchain.prompts.prompt import PromptTemplate
+from langchain.globals import set_llm_cache
+from langchain.cache import InMemoryCache
+
+
+set_llm_cache(InMemoryCache())
 
 chat = ChatOpenAI(
     temperature=0.5,
@@ -10,3 +15,5 @@ chat = ChatOpenAI(
         StreamingStdOutCallbackHandler(),
     ],
 )
+
+chat.predict("How do you make italian pasta")
